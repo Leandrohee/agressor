6c1c5e4a9a002ae21e016249c8994180e680ddec
unknown <leandrohenrique_@live.com>
2023-09-15 18:21:03 -0300

    fiz a funcao pegaHoraprofit

--- agressaoTr.py
+++ agressaoTr.py
@@ -1,11 +1,9 @@
 import pyautogui as pa
 import pytesseract as tes
+import datetime
 
 # Mostrando o caminho do teseract.exe necessario no windows
 tes.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
-
-
 
 # try:
 #     while True:
@@ -15,3 +13,15 @@ tes.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 #         print('\b' * len(positionStr), end='', flush=True)
 # except KeyboardInterrupt:
 #     print('\n')
+
+def pegaHoraProfit():
+    #coordenadas do print
+    sex, sey = 980,550      #superior esquerdo x e y
+    idx, idy = 1270,620      #inferior direito x e y
+    shot = pa.screenshot(region=(sex,sey,idx-sex,idy-sey))   #tira o screenshot do pedaco selecionado
+    shot.save("tempo.png")
+    tempo = tes.image_to_string(shot, config=r"--psm 6")[:8]
+    tempoFormatado = datetime.datetime.strptime(tempo, "%H:%M:%S").time()
+    return tempoFormatado
+
+print(pegaHoraProfit())
